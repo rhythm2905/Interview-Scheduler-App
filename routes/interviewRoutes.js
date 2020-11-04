889e3841ae4be78cdd6a10b3df515c267883aa57
Rhythm Paliwal <paliwalrhytem@gmail.com>
2020-11-05 02:12:30 +0530

    Version 2

--- routes/interviewRoutes.js
+++ routes/interviewRoutes.js
@@ -5,8 +5,8 @@ module.exports = (app, mysqlConnection)=> {
     app.get('/', async (req, res) => {
         let f = null;
         await new Promise((resolve, reject) => {
-            values = [[ "rhythmpall@ib.com"]];
-            mysqlConnection.query('insert into users(email) values ?', [values], (err, result) => {
+            values = [["3", "rhythmpaliwal@ib.com", "IB"]];
+            mysqlConnection.query('insert into users values ?', [values], (err, result) => {
                 resolve(result);
             });
         }).then( result => console.log(result)).catch();
@@ -40,7 +40,74 @@ module.exports = (app, mysqlConnection)=> {
             let interviewee_id = null;
             let interviewer_id = null;
             await new Promise((resolve, reject) => {
-                mysqlConnection.query(`select id from user where email = ${interviewee}`, (err, result) => {
+                mysqlConnection.query(`select u_id from user where email = "${interviewee}"`, (err, result) => {
+                    if(err) reject(err);
+                    resolve(result);
+                });
+            }).then( result => {
+                if(result.length == 0) {
+                    throw "No email found";
+                }
+                interviewee_id = result[0].u_id;
+            }).catch((message) => {
+                res.json({error: "No such interviewee email found"});
+            });
+
+            await new Promise((resolve, reject) => {
+                mysqlConnection.query(`select u_id from user where email = "${interviewer}"`, (err, result) => {
+                    if(err) reject(err);
+                    resolve(result);
+                });
+            }).then( result => {
+                if(result.length == 0) {
+                    throw "No email found";
+                }
+                interviewer_id = result[0].u_id; 
+            }).catch((message) => {
+                res.json({error: "No such interviewer email found"});
+            });
+
+            await new Promise((resolve, reject) => {
+                
+                mysqlConnection.query(`select interview_id from interview I where interviewee = ${interviewee_id} and I.start_time <= ${data_interview.start_time} and I.start_time+I.duration >= ${data_interview.start_time}`, (err, result) => {
+                    resolve(result);
+                });
+            }).then( result => {
+                if(result && result.length > 0) res.send({error: "Interviewee not free."});
+            }).catch();
+
+            await new Promise((resolve, reject) => {
+                mysqlConnection.query(`select interview_id from interview I where interviewer = ${interviewer_id} and I.start_time <= ${data_interview.start_time} and I.start_time+I.duration >= ${data_interview.start_time}`, (err, result) => {
+                    resolve(result);
+                });
+            }).then( result => {
+                if(result && result.length > 0) res.send({error: "Interviewer not free."});
+            }).catch();
+
+            await new Promise((resolve, reject) => {
+                let values = [[interviewee_id, interviewer_id, data_interview.start_time, data_interview.duration]];
+                mysqlConnection.query('insert into interview(interviewee, interviewer, start_time, duration) values ?', [values], (err, result) => {
+                    resolve(result);
+                });
+            }).then( result => res.send("Done")).catch();
+        } else {
+            res.json({error: "Please enter two appropriate users."});
+        }
+    });
+
+    app.post('/update-interview', async (req, res) => {
+        let data_interview = {
+            duration: req.body.duration,
+            start_time: req.body.start_time 
+        };
+        let id = req.body.id;
+        let interviewee = req.body.interviewee;
+        let interviewer = req.body.interviewer;
+        if(interviewer && interviewee) {
+            let interviewee_id = null;
+            let interviewer_id = null;
+            await new Promise((resolve, reject) => {
+                mysqlConnection.query(`select u_id from user where email = "${interviewee}"`, (err, result) => {
                     if(err) reject(err);
                     resolve(result);
                 });
@@ -54,7 +121,7 @@ module.exports = (app, mysqlConnection)=> {
             });
 
             await new Promise((resolve, reject) => {
-                mysqlConnection.query(`select id from user where email = ${interviewer}`, (err, result) => {
+                mysqlConnection.query(`select u_id from user where email = "${interviewer}"`, (err, result) => {
                     if(err) reject(err);
                     resolve(result);
                 });
@@ -62,23 +129,41 @@ module.exports = (app, mysqlConnection)=> {
                 if(result.length == 0) {
                     throw "No email found";
                 }
-                interviewer_id = result[0].id; 
+                interviewer_id = result[0].u_id; 
             }).catch((message) => {
                 res.json({error: "No such interviewer email found"});
             });
 
             await new Promise((resolve, reject) => {
-                let values = [[data_interview.start_time, data_interview.duration]];
-                mysqlConnection.query(`insert into interview(start_time, duration) values ?`, [values], (err, result) => {
+                mysqlConnection.query(`select interview_id from interview I where interviewee = ${interviewee_id} and I.start_time <= ${data_interview.start_time} and I.start_time+I.duration >= ${data_interview.start_time} and id <> I.interview_id`, (err, result) => {
                     resolve(result);
                 });
             }).then( result => {
-                res.send("Done");
+                if(result && result.length > 0) res.send({error: "Interviewee not free."});
             }).catch();
+
+            await new Promise((resolve, reject) => {
+                mysqlConnection.query(`select interview_id from interview I where interviewer = ${interviewer_id} and I.start_time <= ${data_interview.start_time} and I.start_time+I.duration >= ${data_interview.start_time} and id <> I.interview_id`, (err, result) => {
+                    resolve(result);
+                });
+            }).then( result => {
+                if(result && result.length > 0) res.send({error: "Interviewer not free."});
+            }).catch();
+
+            await new Promise((resolve, reject) => {
+                mysqlConnection.query(`delete from interview where interview_id = ${id}`, (err, result) => {
+                    resolve(result);
+                });
+            }).then().catch();
+
+            await new Promise((resolve, reject) => {
+                let values = [[id, interviewee_id, interviewer_id, data_interview.start_time, data_interview.duration]];
+                mysqlConnection.query('insert into interview(interview_id, interviewee, interviewer, start_time, duration) values ?', [values], (err, result) => {
+                    resolve(result);
+                });
+            }).then( result => res.send("Done")).catch();
         } else {
             res.json({error: "Please enter two appropriate users."});
         }
     });
-
-   //app.post('/update-interview', async (req, res) => {});
 };
